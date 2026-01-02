d431c23dcd87fc1ece0f8a1b626c6582664bfecb
ethano1323 <ethano1323@yahoo.com>
2026-01-01 21:41:43 -0600

    Add calculate_course_fit function for score calculation
    This function calculates a course-fit score by adjusting skill weights according to course demands and sorting the results.

--- model/course.fit.py
+++ model/course.fit.py
@@ -1,1 +1,24 @@
+def calculate_course_fit(df, weights):
+    """
+    Calculates course-fit score by anchoring to base_sg
+    and reweighting skill distribution based on course demands.
+    """
+
+    skill_total = (
+        df["sg_app"] +
+        df["sg_ott"] +
+        df["sg_atg"] +
+        df["sg_putt"]
+    )
+
+    df["skill_weighted"] = (
+        df["sg_app"]  * weights["app"] +
+        df["sg_ott"]  * weights["ott"] +
+        df["sg_atg"]  * weights["atg"] +
+        df["sg_putt"] * weights["putt"]
+    )
+
+    df["course_fit"] = df["base_sg"] * (df["skill_weighted"] / skill_total)
+
+    return df.sort_values("course_fit", ascending=False)
 
